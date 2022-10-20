d3fbe5a77568576fd792b9f2209d2b7270b778ca
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-10-20 11:27:30 -0300

    Componentização

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+import entry from "../../assets/entry.svg";
+import out from "../../assets/out.svg";
+import total from "../../assets/total.svg";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entry} alt="entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={out} alt="saidas" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+
+      <div className="hightlight-background">
+        <header>
+          <p>Entradas</p>
+          <img src={total} alt="total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
